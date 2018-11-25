65bc58e2dc86b31d1a5da7204e3729a127ebb943
Kristian Holdhus <45343987+kristianholdhus@users.noreply.github.com>
2018-11-25 14:57:52 -0800

    Switch to JpaRepository in order to support #findAll queries using ExampleMatchers.

--- developer_test_server/java/src/main/java/holdhus/developer_test/repository/FilmRepository.java
+++ developer_test_server/java/src/main/java/holdhus/developer_test/repository/FilmRepository.java
@@ -1,12 +1,12 @@
 package holdhus.developer_test.repository;
 
-import org.springframework.data.repository.CrudRepository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import holdhus.developer_test.entity.Film;
 
-// This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
-// CRUD refers Create, Read, Update, Delete
+// This will be AUTO IMPLEMENTED by Spring into a Bean called filmRepository
 
-public interface FilmRepository extends CrudRepository<Film, Integer> {
+public interface FilmRepository extends JpaRepository<Film, Integer> {
 
 }
